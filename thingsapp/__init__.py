9292df19c0a54b46c2e1f1c54ebe84d947112a26
srid <sridhar.ratna@gmail.com>
2009-10-13 06:56:45 +0530

    dump logbook and all the projects

--- thingsapp/__init__.py
+++ thingsapp/__init__.py
@@ -8,11 +8,25 @@ LOG = logging.getLogger(__name__)
 
 def play():
     from appscript import app, k
-    logbook = app('Things').lists['LogBook'].to_dos()
-    LOG.debug('retrieved Things data')
+    Things = app('Things')
+    LOG.debug('initialized things applescript bridge')
+    
+    LOG.info('writing logbook')
+    with open('tmp/logbook.html', 'w') as f:
+        logbook = Things.lists['LogBook'].to_dos()
+        f.write(render_template(
+                'list.html',
+                title='LogBook',
+                to_dos=logbook,
+                k=k))
 
-    print render_template(
-        'list.html',
-        to_dos=logbook,
-        k=k)
-
+    for project in Things.projects():
+        name = project.name()
+        pathname = name.replace('/', '-')
+        LOG.info('writing project %s', name)
+        with open('tmp/project-{0}.html'.format(pathname), 'w') as f:
+            f.write(render_template(
+                    'list.html',
+                    title='Project: {0}'.format(name),
+                    to_dos=project.to_dos(),
+                    k=k))
